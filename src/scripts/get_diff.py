f59ac3fe80556135a58848c0def8ac15e3a4e33b
Diego Ferro <daferrom@gmail.com>
2025-03-12 16:21:53 -0500

    WIP: Testing dif generator

--- src/scripts/get_diff.py
+++ src/scripts/get_diff.py
@@ -1,12 +1,29 @@
 import subprocess
+import os
+
+
+# Get workspace directory from GitHub Actions
+workspace = os.getenv("GITHUB_WORKSPACE", "LOCAL")
+print("Workspace:", workspace)
+
+# Set diff file path
+diff_file_path = os.path.join(workspace, "diff.txt")
+
+# Overwrite diff_file_path if running locally
+if workspace is "LOCAL":
+    print("Running locally...")
+    diff_file_path = "diff.txt"
+
+
+#TODO: This can be converted to a git command
 
 def get_git_diff():
-    """Get the .diff file of the last commit."""
+    """Get the .diff file of the last  HEAD commit and the previous one"""
     diff = subprocess.run(["git", "diff", "HEAD^"], capture_output=True, text=True)
     return diff.stdout
 
 if __name__ == "__main__":
     diff_text = get_git_diff()
-    with open("diff.txt", "w") as f:
+    with open(diff_file_path, "w") as f:
         f.write(diff_text)
     print("Changes Detected on diff.txt")
