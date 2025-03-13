b1017ac18eb3d5bb4d9917a826f91c39e86a5129
Diego Ferro <daferrom@gmail.com>
2025-03-12 19:25:20 -0500

    WIP: debugging diff generator

--- src/scripts/get_diff.py
+++ src/scripts/get_diff.py
@@ -3,14 +3,14 @@ import os
 
 # TODO: Refactor this worskpace validation in an utility single module in that way can be reused in other scripts
 # Get workspace directory from GitHub Actions
-workspace = os.getenv("GITHUB_WORKSPACE", "LOCAL")
+workspace = os.getenv("GITHUB_WORKSPACE", ".")
 print("Workspace before local validation:", workspace)
 
 # Set diff file path
 diff_file_path = os.path.join(workspace, "diff.txt")
 
 # Overwrite diff_file_path if running locally
-if workspace == "LOCAL":
+if workspace == ".":
     print("Running locally...")
     diff_file_path = "diff.txt"
 
