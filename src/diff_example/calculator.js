40eb418aa8698e26cf7a93a8895c82e7122b4b80
Jhonathan Eduardo A <jhonathaneduardoa.@NCO-NTB-070.local>
2025-03-21 13:53:05 -0700

    multiply funtion

---
+++ src/diff_example/calculator.js
@@ -0,0 +1,13 @@
+function sumar(a, b) {
+    console.log(`Sumando ${a} + ${b}`);
+    return a + b;
+}
+
+function restar(a, b) {
+    console.log(`Restando ${a} - ${b}`);
+    return a - b;
+}
+function multiply(a, b) {
+    console.log(`Restando ${a} x ${b}`);
+    return a * b;
+}
